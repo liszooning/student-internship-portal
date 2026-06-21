1386b23a90c01a807fa9bc9eee97bb85750fca6e
Lisa Rout <lisa.07@gmail.com>
2026-06-21 15:20:28 +0530

    in accounts made empty funcs in views, added urls, made a basic model and did migrations'

---
+++ accounts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('student-signup/',views.student_signup,name='student_signup'),
+    path('recruiter-signup/',views.recruiter_signup,name='recruiter_signup'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def student_signup(request):
+    pass
+
+def recruiter_signup(requet):
+    pass
