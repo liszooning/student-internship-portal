698eacc7e5fa8498ef4b3bd67613fd52f0f7f30b
Lisa Rout <lisa.07@gmail.com>
2026-06-21 14:58:11 +0530

    Created homepage and an accounts app

--- internship_portal/urls.py
+++ internship_portal/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from internships import views as int_views 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',int_views.home,name='home'),
 ]
